28ff88a31f6fbd2bf0818a1e68e688542e1e685a
Adam Stragner <adam@stragner.com>
2024-10-02 22:01:48 +0500

    Add some types

--- Sources/EssentialsImplementation/Compatibility.swift
+++ Sources/EssentialsImplementation/Compatibility.swift
@@ -15,3 +15,11 @@ public typealias ContiguousBytes = Foundation.ContiguousBytes
 /// Availble on WIndows / Linux / Darwin
 /// https://github.com/swiftlang/swift-foundation/blob/main/Sources/FoundationEssentials/Error/CocoaError.swift#L190
 public typealias LocalizedError = Foundation.LocalizedError
+
+/// Availble on WIndows / Linux / Darwin
+/// https://github.com/swiftlang/swift-foundation/blob/main/Sources/FoundationEssentials/URL/URL.swift
+public typealias URL = Foundation.URL
+
+/// Availble on WIndows / Linux / Darwin
+/// https://github.com/swiftlang/swift-foundation/blob/main/Sources/FoundationEssentials/URL/URLComponents.swift
+public typealias URLComponents = Foundation.URLComponents
